25959b3f0b1c1416d5539ee1c77602a0e04e3230
Matt <matthew.kramer@uconn.edu>
2019-02-12 11:49:57 -0500

    Add serial communication test

---
+++ Stepper Motor/serialComTest/serialComTest.ino
@@ -0,0 +1,26 @@
+// Write to serial, arduino reads from serial to turn on or off LED
+const int LED_PIN = 13;
+int ledState = LOW;
+
+void setup() {
+  pinMode(LED_PIN,  OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    Serial.print("Received: ");
+    Serial.println(inByte);
+    switch (inByte) {
+      case 48:
+        digitalWrite(LED_PIN, LOW);
+        Serial.println("Turn off");
+        break;
+      case 49:
+        digitalWrite(LED_PIN, HIGH);
+        Serial.println("Blink");
+        break;
+    }
+  }
+}
