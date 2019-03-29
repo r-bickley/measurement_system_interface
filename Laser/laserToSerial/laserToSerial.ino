285ecc0292df79d1508c0dfb41f48e9a880080b3
Matt <matthew.kramer@uconn.edu>
2019-03-29 13:51:01 -0400

    Laser read smoothing

--- Laser/laserToSerial/laserToSerial.ino
+++ Laser/laserToSerial/laserToSerial.ino
@@ -5,6 +5,10 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(analogRead(sensorPin));
+  //int sensorValue = analogRead(A0); // For reading twice to eliminate stray voltages
+  // Or try Smoothing
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  Serial.println(voltage);
   delay(100); // delay between samples in milliseconds
 }
